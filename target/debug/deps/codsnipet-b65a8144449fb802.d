09c24a0254f47ce18056f94786b9b4594a58425c
MichaelProjects <michael.lichtenecker@outlook.com>
2022-02-17 23:51:08 +0100

    inital push

---
+++ target/debug/deps/codsnipet-b65a8144449fb802.d
@@ -0,0 +1,5 @@
+/Users/michael/Documents/Programming/vercise/codsnipet/target/debug/deps/codsnipet-b65a8144449fb802.rmeta: src/main.rs
+
+/Users/michael/Documents/Programming/vercise/codsnipet/target/debug/deps/codsnipet-b65a8144449fb802.d: src/main.rs
+
+src/main.rs:
