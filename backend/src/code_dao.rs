d4ff394e138546d1ba685311d021d58c94db3dfb
MichaelProjects <michael.lichtenecker@outlook.com>
2022-02-25 01:23:19 +0100

    update

--- backend/src/code_dao.rs
+++ backend/src/code_dao.rs
@@ -10,21 +10,21 @@ use mongodb::{
 use rocket::futures::TryStreamExt;
 use std::error::Error;
 
-async fn get_client() -> Result<Client, Box<dyn Error>> {
-    let client_options = ClientOptions::parse("mongodb://user:password123@localhost:27017").await?;
+async fn get_client(db_url: &String) -> Result<Client, Box<dyn Error>> {
+    let client_options = ClientOptions::parse(db_url).await?;
     let client = Client::with_options(client_options)?;
     Ok(client)
 }
 
-pub async fn insert_code_snipet(data: Code) -> Result<bool, Box<dyn Error>> {
-    let database = get_client().await?.database("codsnipet");
+pub async fn insert_code_snipet(db_url: &String, data: Code) -> Result<bool, Box<dyn Error>> {
+    let database = get_client(db_url).await?.database("codsnipet");
     let typed_collection = database.collection::<Code>("snippets");
-    typed_collection.insert_one(data, None).await.unwrap();
+    typed_collection.insert_one(data, None).await?;
     Ok(true)
 }
 
-pub async fn get_code_snipet(id: String) -> Result<Option<Code>, Box<dyn Error>> {
-    let database = get_client().await?.database("codsnipet");
+pub async fn get_code_snipet(db_url: &String, id: String) -> Result<Option<Code>, Box<dyn Error>> {
+    let database = get_client(db_url).await?.database("codsnipet");
     let typed_collection = database.collection::<Code>("snippets");
     
     let parsed_id = id.as_str();
@@ -35,8 +35,8 @@ pub async fn get_code_snipet(id: String) -> Result<Option<Code>, Box<dyn Error>>
     Ok(cursor)
 }
 
-pub async fn update_code_snipet(id: String, payload: CodeIn) -> Result<bool, Box<dyn Error>> {
-    let database = get_client().await?.database("codsnipet");
+pub async fn update_code_snipet(db_url: &String, id: String, payload: CodeIn) -> Result<bool, Box<dyn Error>> {
+    let database = get_client(db_url).await?.database("codsnipet");
     let typed_collection = database.collection::<Code>("snippets");
     
     let parsed_id = id.as_str();
@@ -59,8 +59,8 @@ pub async fn update_code_snipet(id: String, payload: CodeIn) -> Result<bool, Box
     }
 }
 
-pub async fn delete_code_snipet(id: String) -> Result<bool, Box<dyn Error>> {
-    let database = get_client().await?.database("codsnipet");
+pub async fn delete_code_snipet(db_url: &String, id: String) -> Result<bool, Box<dyn Error>> {
+    let database = get_client(db_url).await?.database("codsnipet");
     let typed_collection = database.collection::<Code>("snippets");
     
     let parsed_id = id.as_str();
@@ -76,8 +76,8 @@ pub async fn delete_code_snipet(id: String) -> Result<bool, Box<dyn Error>> {
     }
 }
 
-pub async fn get_all_code_snipets(limit: i32, site: i32) -> Result<Vec<Code>, Box<dyn Error>> {
-    let database = get_client().await?.database("codsnipet");
+pub async fn get_all_code_snipets(db_url: &String, limit: i32, site: i32) -> Result<Vec<Code>, Box<dyn Error>> {
+    let database = get_client(db_url).await?.database("codsnipet");
     let typed_collection = database.collection::<Code>("snippets");
     let skip = limit * site;
     let option_filter = FindOptions::builder().build();
--- backend/src/handle.rs
+++ backend/src/handle.rs
@@ -1,7 +1,9 @@
+use rocket::State;
 use rocket::http::Status;
 use serde_json::json;
 
 use rocket::serde::json::Json;
+use crate::config::ConfigGeneral;
 use crate::models::CodeIn;
 use crate::response::ApiResponse;
 use crate::code::Code;
@@ -9,17 +11,17 @@ use crate::code_dao::{insert_code_snipet, delete_code_snipet, get_code_snipet, g
 
 
 #[post("/code", data = "<payload>")]
-pub(crate) async fn create_entry(payload: Json<CodeIn>) -> ApiResponse{
-    let result = insert_code_snipet(Code::from_code_in(payload.0)).await;
+pub(crate) async fn create_entry(settings: &State<ConfigGeneral>, payload: Json<CodeIn>) -> ApiResponse{
+    let result = insert_code_snipet(&settings.database.connection_string, Code::from_code_in(payload.0)).await;
     match result{
         Ok(code) => ApiResponse::new(Status::Created, json!({"message": "created"})),
-        Err(err) => ApiResponse::new(Status::BadRequest, json!({"error": "failed"}))
+        Err(err) => ApiResponse::new(Status::BadRequest, json!({"error": err.to_string()}))
     }
 }
 
 #[patch("/code/<code_id>", data = "<payload>")]
-pub async fn update_entry_endpoint(code_id: String, payload: Json<CodeIn>) -> ApiResponse{
-    let result = update_code_snipet(code_id, payload.0).await;
+pub async fn update_entry_endpoint(settings: &State<ConfigGeneral>, code_id: String, payload: Json<CodeIn>) -> ApiResponse{
+    let result = update_code_snipet(&settings.database.connection_string, code_id, payload.0).await;
     match result{
         Ok(code) => ApiResponse::new(Status::Ok, json!({"message": "updated!"})),
         Err(err) => ApiResponse::new(Status::NotFound, json!({"error": "failed to update"}))
@@ -27,8 +29,8 @@ pub async fn update_entry_endpoint(code_id: String, payload: Json<CodeIn>) -> Ap
 }
 
 #[delete("/code/<code_id>")]
-pub async fn delete_entry_endpoint(code_id: String) -> ApiResponse{
-    let result = delete_code_snipet(code_id).await;
+pub async fn delete_entry_endpoint(settings: &State<ConfigGeneral>, code_id: String) -> ApiResponse{
+    let result = delete_code_snipet(&settings.database.connection_string, code_id).await;
     match result{
         Ok(code) => ApiResponse::new(Status::Ok, json!({"message": "deleted"})),
         Err(err) => ApiResponse::new(Status::NotFound, json!({"error": "failed deleting snipet"}))
@@ -36,8 +38,8 @@ pub async fn delete_entry_endpoint(code_id: String) -> ApiResponse{
 }
 
 #[get("/code/<code_id>")]
-pub async fn get_entry(code_id: String) -> ApiResponse{
-    let result = get_code_snipet(code_id).await;
+pub async fn get_entry(settings: &State<ConfigGeneral>, code_id: String) -> ApiResponse{
+    let result = get_code_snipet(&settings.database.connection_string, code_id).await;
     match result{
         Ok(code) => ApiResponse::new(Status::Ok, json!({"data": code})),
         Err(err) => ApiResponse::new(Status::NotFound, json!({"error": "Failed"}))
@@ -46,10 +48,11 @@ pub async fn get_entry(code_id: String) -> ApiResponse{
 
 #[get("/code?<limit>&<site>")]
 pub async fn get_all_entries(
+    settings: &State<ConfigGeneral>,
     limit: Option<i32>, 
     site: Option<i32>
 ) -> ApiResponse{
-    let result = get_all_code_snipets(limit.unwrap(), site.unwrap()).await;
+    let result = get_all_code_snipets(&settings.database.connection_string,limit.unwrap(), site.unwrap()).await;
     match result{
         Ok(code) => ApiResponse::new(Status::Ok, json!({"data": code})),
         Err(err) => ApiResponse::new(Status::NotFound, json!({"": ""}))
